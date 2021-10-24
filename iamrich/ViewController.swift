4d458a1875c3cd720618f60efb6ee44111f62a36
yazicie <Js8iM4K7xqKMZ48>
2021-10-24 23:01:42 +0300

    Initial Commit

---
+++ iamrich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iamrich
+//
+//  Created by Ertuğrul YAZICI on 24.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
